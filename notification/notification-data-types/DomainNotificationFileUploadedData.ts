86c6fbdbeb81f885af1d2a1794087bf08c0ea125
VanyaMate <buhu653@mail.ru>
2024-07-15 05:21:43 +0300

    add File notifications

---
+++ notification/notification-data-types/DomainNotificationFileUploadedData.ts
@@ -0,0 +1,28 @@
+import { TypeGuard } from '../../_helpers/types/guard.types';
+import { TypeAssert } from '../../_helpers/types/assert.types';
+import { throwAssertError } from '../../_helpers/lib/throwAssertError';
+import { DomainFile, isDomainFile } from '../../file/DomainFile';
+import { isObject } from '../../_helpers/lib/isObject';
+
+
+export type DomainNotificationFileUploadedData = {
+    file: DomainFile;
+}
+
+export const isDomainNotificationFileUploadedData: TypeGuard<DomainNotificationFileUploadedData> = function (data: unknown): data is DomainNotificationFileUploadedData {
+    if (!isObject(data)) {
+        return false;
+    }
+
+    if (!isDomainFile(data['file'])) {
+        return false;
+    }
+
+    return true;
+};
+
+export const assertDomainNotificationFileUploadedData: TypeAssert<DomainNotificationFileUploadedData> = function (data: unknown, variableName: string, typeName: string) {
+    if (!isDomainNotificationFileUploadedData(data)) {
+        throwAssertError(variableName, typeName);
+    }
+};
